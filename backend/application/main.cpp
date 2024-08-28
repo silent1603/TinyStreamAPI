9e0dcce105dcf175528411f16942efcba0e7ef58
Dao Quoc Trung <trung.daoquoc@gameloft.com>
2024-08-28 09:47:10 +0700

    * update folder structure

--- backend/application/main.cpp
+++ backend/application/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
 
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", 8080);
+    drogon::app().addListener("0.0.0.0", 8085);
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
